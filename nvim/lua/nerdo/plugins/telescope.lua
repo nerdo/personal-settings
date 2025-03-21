790f38c58d6bbe898d0a4a8675484a9d8cecde5f
Dannel Albert <cuebix@gmail.com>
2025-03-21 19:21:49 -0400

    feat: add lazygit, tweak telescope

--- nvim/lua/nerdo/plugins/telescope.lua
+++ nvim/lua/nerdo/plugins/telescope.lua
@@ -13,6 +13,14 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		telescope.setup({
+			defaults = {
+				layout_strategy = 'center',
+				layout_config = {
+					anchor = "N",
+					width = 0.9,
+					height = 0.25,
+				}
+			},
 			pickers = {
 				find_files = {
 					hidden = true,
@@ -21,11 +29,20 @@ return {
 		})
 
 		local builtin = require("telescope.builtin")
+		local opts = require('telescope.themes').get_ivy({
+			cwd = vim.fn.stdpath('config')
+		})
 
 		vim.keymap.set({ "n", "i" }, "<A-p>", builtin.git_files, {})
 		vim.keymap.set({ "n", "i" }, "<C-p>", builtin.find_files, {})
 		vim.keymap.set({ "n", "i" }, "<C-f>", builtin.live_grep, {})
 		vim.keymap.set({ "n", "i" }, "<A-f>", builtin.live_grep, {})
+		vim.keymap.set({ "n", "i" }, "<leader>,", function()
+			-- edit neovim configuration from anywhere
+			builtin.find_files {
+				cwd = vim.fn.stdpath("config")
+			}
+		end, {})
 		vim.keymap.set("n", "<leader>?", "<Cmd>Telescope<CR>")
 		vim.keymap.set("n", "<leader>M", "<Cmd>Telescope notify<CR>")
 
