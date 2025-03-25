312fc213353c318582738e337520238c9c363e47
Dannel Albert <cuebix@gmail.com>
2025-03-25 17:55:32 -0400

    feat: add trouble.nvim

---
+++ nvim/lua/nerdo/plugins/trouble.lua
@@ -0,0 +1,54 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      project = {
+        mode = 'diagnostics',
+        filter = {
+          any = {
+            buf = 0,
+            {
+              severity = vim.diagnostic.severity.ERROR,
+              function(item)
+                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble project toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}
