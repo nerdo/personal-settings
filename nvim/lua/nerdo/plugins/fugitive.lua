91aa2be5b812c19543b106df782ed6a5c71bbd61
Dannel Albert <cuebix@gmail.com>
2026-02-13 11:00:03 -0500

    fix: add dummy winbar to fugitive blame/diff and gitsigns diff windows to fix off-by-1 alignment with lspsaga breadcrumb

--- nvim/lua/nerdo/plugins/fugitive.lua
+++ nvim/lua/nerdo/plugins/fugitive.lua
@@ -1,4 +1,22 @@
 -- Git tools.
 return {
 	"tpope/vim-fugitive",
+	config = function()
+		-- Set a dummy winbar on fugitive blame and diff windows so they align
+		-- with the source window's lspsaga breadcrumb winbar (prevents off-by-1).
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "fugitiveblame",
+			callback = function()
+				vim.wo.winbar = " "
+			end,
+		})
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			callback = function()
+				local bufname = vim.api.nvim_buf_get_name(0)
+				if bufname:match("^fugitive://") then
+					vim.wo.winbar = " "
+				end
+			end,
+		})
+	end,
 }
--- nvim/lua/nerdo/plugins/gitsigns.lua
+++ nvim/lua/nerdo/plugins/gitsigns.lua
@@ -8,6 +8,17 @@ return {
 	config = function()
 		local gitsigns = require("gitsigns")
 
+		-- Set a dummy winbar on gitsigns diff buffers so they align with
+		-- the source window's lspsaga breadcrumb winbar (prevents off-by-1).
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			callback = function()
+				local bufname = vim.api.nvim_buf_get_name(0)
+				if bufname:match("^gitsigns://") then
+					vim.wo.winbar = " "
+				end
+			end,
+		})
+
 		gitsigns.setup({
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
